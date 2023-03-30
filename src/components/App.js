ebc688ec0ef0b3fb9134e3a5358f350b32d347c0
kautik k sonawane <111452796+2044kautik@users.noreply.github.com>
2023-03-30 16:40:24 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./NavBar";
+import SearchBus from "./SearchBus";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import SearchResult from "./SearchResult";
+import BusContext from "./BusContext";
+import {useState} from "react"
+import SeatSelection from "./SeatSelection";
+import Journey from "./Journey";
+import Login from "./Login";
 
-
+function App() {
+  const [from, setFrom] =useState("")
+  const [to, setTo] = useState("")
+  const [buses, setBuses] = useState([])
+  const [busLoader, setBusLoader] = useState(false)
   return (
-    <div id="main">
+    <div className="App">
+      <BusContext.Provider value={{from, to, setFrom, setTo, buses, setBuses, busLoader, setBusLoader}}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="/" element={<Journey Component={Home}></Journey>}></Route>
+          <Route
+            path="/results"
+            element={<Journey Component={SearchResult}></Journey>}
+          ></Route>
+          <Route
+            path="/book-seats"
+            element={<Journey Component={SeatSelection}></Journey>}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+      </BusContext.Provider>
     </div>
-  )
+  );
 }
-
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import {Image} from "react-bootstrap"
+
+function Home(){
+    return (<div>
+        <Image className="w-100" src="https://st.redbus.in/seo/ind/HeroImageGuideline-V-2-new_1650873408.jpg"></Image>
+    </div>)
+}
+
+export default Home
