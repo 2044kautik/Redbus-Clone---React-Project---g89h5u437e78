7c70e6ece19a83c2eef64621373a4faca264828d
kautik k sonawane <111452796+2044kautik@users.noreply.github.com>
2023-05-26 16:37:18 +0530

    Delete src directory

--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import {Image} from "react-bootstrap"
-
-function Home(){
-    return (<div>
-        <Image className="w-100" src="https://st.redbus.in/seo/ind/HeroImageGuideline-V-2-new_1650873408.jpg"></Image>
-    </div>)
-}
-
-export default Home
